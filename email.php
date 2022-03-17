12cc011f8aa6ea3492260c206f28051afab03752
AngelAlgo <angelocampo262@gmail.com>
2022-03-17 09:13:52 -0600

    Haciendo Cambios en Contacto

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 $subject = 'You Got Message'; // Subject of your email
-$to = 'info@example.com';  //Recipient's E-mail
+$to = 'jocampo@w32.mx';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $name = $_REQUEST['name'];
